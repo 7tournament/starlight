88a5e1bb1a5a6129245803df79270e8011676867
Tournament <e1cooperamirr@gmail.com>
2026-01-16 00:05:09 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("explodeBtn");
-
-btn.addEventListener("click", () => {
-  btn.classList.add("explode");
-
-  setTimeout(() => {
-    window.location.href = "quotes.html";
-  }, 800);
-});
